15af223bf37edc868a628266d99d3acee0556a5d
AirRice <jacob9909@naver.com>
2024-02-22 13:24:38 +0000

    Finish implementation of SharedScan

--- Assets/Scripts/Bots/SharedScan.cs
+++ Assets/Scripts/Bots/SharedScan.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public class SharedScan : BotTemplate
 {
-    public static List<int> possibleLocations = new List<int>(); //Node ID list
+    public static Dictionary<int,bool> possibleLocations = new(); //Node ID dict: int node ID, bool is possible.
         
     public override void ProcessTurn(int playerID, int currentNodeID, int actionsLeft)
     {
@@ -19,39 +19,67 @@ public class SharedScan : BotTemplate
         }
         else if(playerID == 1)
         {
-            
+            // Reset this dict when a new round starts
+            if (gcr.turnCount == 0)
+            {
+                possibleLocations = new();
+            }
+
+            //Make this list for the first time if it doesn't exist yet. 
+            //Add the hidden player's spawn if it's the first time (first turn expected)
+            if (SharedScan.possibleLocations.Count == 0)
+            {
+                for ( int i = 1; i <= mapSizeSq; i++ )
+                {
+                    SharedScan.possibleLocations.Add(i, false);
+                }
+                SharedScan.possibleLocations[gcr.hiddenSpawn.nodeID] = true;
+            }
+
+            int[] locationsToPropagate = (from kvp in SharedScan.possibleLocations where kvp.Value select kvp.Key).ToArray();
+            //If a node was infected add the surrounding nodes to the possible locations info
+            //This can be up to (max movement limit - distance to last infected node from closest last possible node) distance away
+            int lastInfected = gcr.lastInfectedNode;
+            if (lastInfected < mapSizeSq && lastInfected > 0 )
+            {
+                foreach(int nodeID in gcr.GetAdjacentNodes(lastInfected,3))
+                {
+                    SharedScan.possibleLocations[nodeID] = true;
+                }
+            }
+            else
+            {
+                //Grow the search space size if a turn has passed, because the hidden player may have moved.
+                foreach(int locID in locationsToPropagate)
+                {
+                    foreach(int nodeID in gcr.GetAdjacentNodes(locID, 3))
+                    {
+                        SharedScan.possibleLocations[nodeID] = true;
+                    }
+                }
+            }
+
+            Debug.Log(string.Join(", ", (from kvp in SharedScan.possibleLocations where kvp.Value select kvp.Key).ToArray()));
         } 
         int currentLocation = currentNodeID;
         
         /**
         This algorithm works in the following order.
-        1. If no previous scan information/newly infected nodes exist for this turn, scan to gain some information.
-        2. Using the info, locate all potential locations the hidden player could be in at the current moment. 
-           Triangulate possible locations from all known info that turn.
-        3. Randomly select one of the possible locations.
-        4. Head to the location and scan the node, if it is possible to do so (if it is within range)
-        5. If the scanning has not resulted in a scanner victory, try and repeat step 3-4 with another possible location.
-        6. Do this until the action allocation runs out.
-        7. When the hidden player's turn ends, add to the possible locations list all nodes of 3 distance away from previously possible locations
+        1. Triangulate all potential locations from previous scan information, newly infected nodes this turn, and hidden player spawn position
+        2. Randomly select one of the possible locations.
+        3. Head to the location and scan the node, if it is possible to do so (if it is within range)
+        4. If the scanning has not resulted in a scanner victory, try and repeat step 2-3 with another possible location. Do this until the action allocation runs out.
+        5. When the hidden player's turn ends, add to the possible locations list all nodes of 3 distance away from previously possible locations
            and nodes up to (max movement limit - distance to last infected node from closest last possible node) distance away
            from the last infected node if one was infected last turn.
         **/
 
         while(actionsLeft > 0)
         {
-            //Make this list for the first time if it doesn't exist yet.
-            if (SharedScan.possibleLocations.Count == 0)
-            {
-                for(int i = 1; i <= mapSizeSq; i++)
-                {
-                    SharedScan.possibleLocations.Add(i);
-                }
-            }
             int scanTarget = -1;
             List<(int,int[])> prevScans = gcr.scanHistory;
-            int lastInfected = gcr.lastInfectedNode;
-            // If no previous scan info exists for the turn, do one just for some information
-            if (prevScans.Count == 0 || lastInfected > gcr.mapSize * gcr.mapSize || lastInfected < 0 )
+            // If no info exists for the turn, do one just for some information
+            if ((from kvp in SharedScan.possibleLocations where kvp.Value select kvp.Key).ToList().Count <= 0)
             {
                 GameController.gameController.TrySpecialAction();
                 Debug.Log($"Scanning as first action");
@@ -66,74 +94,59 @@ public class SharedScan : BotTemplate
                         // Ignore this 
                         //continue;
                     Debug.Log($"Previous scan info: Hidden player was in direction(s) of Node(s) {string.Join(" and ",info.Item2)} from starting node {info.Item1}");
-                    for (int i = SharedScan.possibleLocations.Count-1; i >=0 ; i--)
+                    int[] possLocs = (from kvp in SharedScan.possibleLocations where kvp.Value select kvp.Key).ToArray();
+                    foreach (int curNode in possLocs)
                     {
-                        int curNode = SharedScan.possibleLocations[i];
                         bool nodeflag = false;
                         int[] pathto = gcr.GetCappedPath(curNode,info.Item1);
-                        int nodeOfDir = info.Item2[0];
+                        Debug.Log(string.Join(", ", pathto));
                         // Delete the node from the list if it's not in the direction of the given node + equal distance away from all given nodes (if multiple exist)
-                        if(pathto.Contains(nodeOfDir) && info.Item2.All(id => gcr.GetPathLength(id,curNode) == gcr.GetPathLength(nodeOfDir,curNode)))
+                        foreach (int nodeOfDir in info.Item2)
                         {
-                            nodeflag = true;
+                            if(pathto.Contains(nodeOfDir) && info.Item2.All(id => gcr.GetPathLength(id,curNode) == gcr.GetPathLength(nodeOfDir,curNode)) && !gcr.infectedNodeIDs.Contains(curNode))
+                            {
+                                nodeflag = true;
+                            }
                         }
                         if (!nodeflag)
-                            SharedScan.possibleLocations.RemoveAll(r => r == curNode);
+                            SharedScan.possibleLocations[curNode] = false;
                     }
                 }
                 
-                //Copy the locations list
-                List<int> possibleLocations_temp = possibleLocations;
-                bool flag = false;
-                while(!flag && possibleLocations_temp.Count > 0)
-                {
-                    //use random iteration to decide on a potential target.
-                    int rand_index = Random.Range(0, possibleLocations_temp.Count);
-                    int potential_target = possibleLocations_temp[rand_index];
-                    if((gcr.GetPathLength(currentLocation,potential_target) < actionsLeft)
-                        || (currentLocation == potential_target && actionsLeft >= 2))
-                    {
-                        scanTarget = potential_target;
-                        Debug.Log($"Potential Target found: node id {scanTarget}");
-                        flag = true;
-                    }
-                    else
-                    {
-                        possibleLocations_temp.Remove(potential_target);
-                    }
+                //Get an intersection of possible targets vs our movement range and select one randomly
+                int[] possibleLocations_temp = (from kvp in SharedScan.possibleLocations where kvp.Value select kvp.Key).ToArray();
+                int[] nodesInRange = gcr.GetAdjacentNodes(currentLocation,actionsLeft-1);
+                List<int> targetLocs = nodesInRange.Intersect(possibleLocations_temp).ToList();
+                int fallbackTarget = -1;
+                if (targetLocs.Count > 0)
+                {              
+                    int rand_index = Random.Range(0, targetLocs.Count);
+                    Debug.Log(string.Join(", ", targetLocs) + $" selecting element number {rand_index}");
+                    scanTarget = targetLocs[rand_index];
+                    Debug.Log($"Potential Target found: node id {scanTarget}");
                 }
+                if (possibleLocations_temp.Count() > 0)
+                {
+                    int rand_index_fallback = Random.Range(0, possibleLocations_temp.Count());
+                    fallbackTarget = possibleLocations_temp[rand_index_fallback];
+                }
+
+                // Move to the node and scan it if valid; If not then move to one of the further ones
                 if(scanTarget!=-1)
                 {
-                    //scan is only possible on adjacent spaces
-                    //Edge case: Scan target is same space as current node
-                    if(currentLocation == scanTarget)
-                    {
-                        int toMoveTo = SelectNextNodeRandom(currentLocation);
-                        Debug.Log($"Moving to node id {toMoveTo}");
-        
-                        gcr.TryMoveToNode(toMoveTo);
-                        currentLocation = toMoveTo;
-                        actionsLeft--;
-                    }
-                    else
-                    {
-                        int movedist = gcr.GetPathLength(currentLocation,scanTarget);
-                        int[] toPrevNode = gcr.GetCappedPath(currentLocation,scanTarget,movedist-1);
-                        int prevNode = toPrevNode[^1];
-                        Debug.Log($"Scan target {scanTarget}, Previous node {prevNode}");
-                        
-                        gcr.TryMoveToNode(prevNode);
-                        currentLocation = prevNode;
-                        Debug.Log($"Moving to node id {prevNode}");
-                        actionsLeft-=movedist-1;
-                    }
+                    
+                    Debug.Log($"Moving to Scan target {scanTarget}");
+                    int finalLocation = gcr.TryMoveToNode(scanTarget);
+                    actionsLeft-=gcr.GetPathLength(currentLocation,finalLocation);
+                    currentLocation = finalLocation;
+
                     gcr.TrySpecialAction(Node.GetNode(scanTarget));
                     Debug.Log($"Scanning node id {scanTarget}");
                     actionsLeft--;
                 }
-                else
+                else if (fallbackTarget > 0 && fallbackTarget < mapSizeSq)
                 {
-                    int toMoveTo = SelectNextNodeRandom(currentLocation);
+                    int toMoveTo = fallbackTarget;
                     Debug.Log($"Moving to node id {toMoveTo}");
     
                     gcr.TryMoveToNode(toMoveTo);
