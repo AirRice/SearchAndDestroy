60c7070616b6923c695f25f1cfadfc419ed1d823
AirRice <jpumph2@gmail.com>
2024-03-19 15:28:00 +0000

    saving to cfg

--- Assets/Scripts/LoadData.cs
+++ Assets/Scripts/LoadData.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class LoadData : MonoBehaviour
 {
+    private static readonly string filePath = System.IO.Path.Combine(Application.dataPath, "config.json");
     public static ConfigData Load(){
-        string filePath = System.IO.Path.Combine(Application.dataPath, "config.json");
+        
         Debug.Log(filePath);
         string data = System.IO.File.ReadAllText(filePath);
-        
         ConfigData cfgData = JsonUtility.FromJson<ConfigData>(data);
         return cfgData;
+    }
+    public static void Save(ConfigData cfgData)
+    {
+        string jsonString = JsonUtility.ToJson(cfgData);
+        System.IO.File.WriteAllText(filePath, jsonString);
     }
 }
 
@@ -26,4 +31,29 @@ public class ConfigData{
     public bool logToCSV;
     public bool useSmoothMove;
     public string[] playerBotType;
+
+    public ConfigData(
+        int mapSize,
+        int playersCount,
+        int movesCount,
+        int maxTurnCount,
+        int maxRoundCount,
+        int maxObjectives,
+        bool hotSeatMode,
+        bool logToCSV,
+        bool useSmoothMove,
+        string[] playerBotType
+    )
+    {
+        this.mapSize = mapSize;
+        this.playersCount = playersCount;
+        this.movesCount = movesCount;
+        this.maxTurnCount = maxTurnCount;
+        this.maxRoundCount = maxRoundCount;
+        this.maxObjectives = maxObjectives;
+        this.hotSeatMode = hotSeatMode;
+        this.logToCSV = logToCSV;
+        this.useSmoothMove = useSmoothMove;
+        this.playerBotType = playerBotType;
+    }
 }
